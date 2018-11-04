5741ff560f30d38bbe7867e617ef1e8514c1de95
daylen <daylen3@uw.edu>
2018-11-03 17:22:50 -0700

    fix

--- base/CONSTANTS.py
+++ base/CONSTANTS.py
@@ -1,6 +1,6 @@
 HEADER = "Alias, URL, Children"
-URL_FILE = './urls3.txt'
-OUTPUT_CSV_FILE = "output.csv"
+URL_FILE = '../url/urls3.txt'
+OUTPUT_CSV_FILE = "../output/output.csv"
 MSG_INVALID_TEXT = "invalid txt"
 CHILD_FIRST_OCCURANCE = -1
 PROC_LINKS_MAX = 75
